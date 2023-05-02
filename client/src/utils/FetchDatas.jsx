d6d51c9b8e30cd25880532e29245e7eb16796f22
mouhamed <mouhamed.mbaye1@outlook.com>
2023-05-03 00:39:11 +0200

    updt fetchData again

--- client/src/utils/FetchDatas.jsx
+++ client/src/utils/FetchDatas.jsx
@@ -5,7 +5,7 @@ import { dataBase } from "../data/db";
 /* This code defines a custom React hook called `FetchDatas` that takes in two parameters: `url` and
 `id`. It uses the `useState` and `useEffect` hooks to manage state and perform side effects
 respectively. */
-const REACT_MOCKED_DATA = false;
+const MOCKED_DATA = false;
 
 
 const FetchDatas = (url, id) => {
@@ -13,7 +13,7 @@ const FetchDatas = (url, id) => {
 
     useEffect(() => {
 
-        if (REACT_MOCKED_DATA) {
+        if (MOCKED_DATA) {
             setUserData(dataBase.find((data) => { return data.id === parseInt(id) }))
         } else {
             fetch(url)
